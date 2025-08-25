0db4830c50cdac243273be80d087f40ffd10a3ca
Jitesh Prajapati <jiteshprajapati@Jiteshs-MacBook-Air.local>
2025-08-25 19:25:55 +0530

    changes added for responsive website

--- src/components/MovieSuggestionResult.js
+++ src/components/MovieSuggestionResult.js
@@ -3,7 +3,7 @@ import MoveList from "./MovieList";
 
 const MovieSuggestionResult = () => {
     const {suggestedMovies, movieName} = useSelector(state => state.suggestionSearch)
-    if(!movieName) return 'No Result found';
+    if(!movieName) return null;
 
     return (
         <div className="p-4 m-4 text-white searchSuggestedList">
